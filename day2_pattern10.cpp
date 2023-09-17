57026dac25f3abcbd5aa29628ad3ab6b3efb16fa
Aditya9600ghosh <130989727+Aditya9600ghosh@users.noreply.github.com>
2023-09-17 19:57:54 +0530

    Create day2_pattern10.cpp

---
+++ day2_pattern10.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int r;
+    cin>>r;
+    for(int i=1;i<=r;i++)
+    {
+        for(int j=1;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=i;)
+    }
+}
